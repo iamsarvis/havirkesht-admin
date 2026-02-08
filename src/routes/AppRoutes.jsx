7458b509c476169a98a352ad076f0e5b87dc6c7d
Sobhan <iamsarvis@gmail.com>
2026-02-08 12:01:09 +0330

    add farmer page to route handler

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ProvinceList from "../pages/province/ProvinceList";
+import FarmerList from "../pages/farmer/FarmerList";
 import AdminLayout from "../components/layout/AdminLayout";
 import ProtectedRoute from "./ProtectedRoute";
 
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route element={<AdminLayout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/province" element={<ProvinceList />} />
+          <Route path="/farmer" element={<FarmerList />} />
         </Route>
     </Route>
     </Routes>
